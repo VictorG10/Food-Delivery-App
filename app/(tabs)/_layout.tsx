3966065cca9d84a5b6b9dea970b8ce46503ed9b8
VictorG10 <victorgbadebo10@gmail.com>
2025-07-09 02:48:51 +0100

    adding tab navigation and auth folder set up

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Redirect, Tabs } from "expo-router";
+import React from "react";
+
+const TabsLayout = () => {
+  const isAuthenticated = true;
+
+  if (!isAuthenticated) return <Redirect href={"/sign-in"} />;
+
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="cart" options={{ title: "Cart" }} />
+      <Tabs.Screen name="search" options={{ title: "Search" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
+
+// 1:05:31
---
+++ app/(tabs)/cart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const CartScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>CartScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default CartScreen;
